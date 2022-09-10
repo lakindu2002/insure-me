9fc9775719306f1d4c155f40f3d1715ce8cd2c7e
lakindu hewawasam <lakindu2002@live.com>
2022-09-10 17:20:08 +0530

    fixing import errors

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,21 @@
 module.exports = {
-  presets: [
-      ['module:metro-react-native-babel-preset', { useTransformReactJSXExperimental: true }],
-  ],
-  plugins: [
-      [
-          '@babel/plugin-transform-react-jsx',
-          {
-              runtime: 'automatic',
-          },
-      ],
-  ],
+    presets: [
+        ['module:metro-react-native-babel-preset', { useTransformReactJSXExperimental: true }],
+    ],
+    plugins: [
+        [
+            '@babel/plugin-transform-react-jsx',
+            {
+                runtime: 'automatic',
+            },
+        ],
+        [
+            'module-resolver',
+            {
+                alias: {
+                    '@insureme': './src',
+                },
+            },
+        ],
+    ],
 };
