dccdfd3062cefb8c0f5639dbc263a7489ac3d33d
lakindu hewawasam <lakindu2002@live.com>
2022-09-11 17:17:35 +0530

    adding helper text

--- src/common/OutlinedTextInput.tsx
+++ src/common/OutlinedTextInput.tsx
@@ -1,13 +1,24 @@
-import React, { FC } from 'react';
-import { TextInput } from 'react-native-paper';
+import React, { FC, Fragment } from 'react';
+import { HelperText, TextInput } from 'react-native-paper';
 import { TextInputProps } from 'react-native-paper/lib/typescript/components/TextInput/TextInput';
 
-interface OutlinedTextInputProps extends Partial<TextInputProps> { }
+interface OutlinedTextInputProps extends Partial<TextInputProps> {
+  helperText?: string | false | undefined;
+}
 
-export const OutlinedTextInput: FC<OutlinedTextInputProps> = (props) => {
+export const OutlinedTextInput: FC<OutlinedTextInputProps> = ({ helperText, ...props }) => {
   return (
-    <TextInput {...props}
-      mode='outlined'
-    />
+    <Fragment>
+      <TextInput {...props}
+        mode='outlined'
+      />{
+        !!helperText && (
+          <HelperText type={props.error ? 'error' : 'info'} visible={!!helperText}>
+            {helperText}
+          </HelperText>
+        )
+      }
+
+    </Fragment>
   );
 };
