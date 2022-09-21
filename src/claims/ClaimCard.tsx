c3d95510a2a15208aff5e4a3bcad446e945dbbed
lakindu hewawasam <lakindu2002@live.com>
2022-09-22 00:37:19 +0530

    added claim card

---
+++ src/claims/ClaimCard.tsx
@@ -0,0 +1,50 @@
+import { CustomButton } from '@insureme/common/CustomButton';
+import moment from 'moment';
+import React, { FC } from 'react';
+import { Card, Text, useTheme } from 'react-native-paper';
+import { Claim } from './Claim.type';
+import { getAccidentTypeName } from './Claim.util';
+
+interface ClaimCardProps {
+  claim: Claim;
+  onMoreDetailsPress: () => void;
+}
+
+export const ClaimCard: FC<ClaimCardProps> = React.memo((props) => {
+  const { claim, onMoreDetailsPress } = props;
+  const { accidentType, vehicle, time, user } = claim;
+  const { name, phone } = user;
+  const theme = useTheme();
+
+  const accidentTypeName = getAccidentTypeName(accidentType, 'Accident');
+
+  return (
+    <Card>
+      <Card.Title title={accidentTypeName} />
+      <Card.Content>
+        <Text style={{
+          color: theme.colors.disabled,
+          fontWeight: '600'
+        }}>
+          {vehicle.brand} {vehicle.model} | {name} - {phone}
+        </Text>
+        <Text style={{ marginTop: 5 }}>
+          Accident Time: {moment(time).format('DD MMM YYYY, hh:mm a')}
+        </Text>
+      </Card.Content>
+      <Card.Actions style={{
+        paddingLeft: 12,
+        paddingBottom: 15,
+        marginLeft: 'auto'
+      }}>
+        <CustomButton
+          label='View Details'
+          mode='contained'
+          onPress={onMoreDetailsPress}
+          labelStyle={{ fontSize: 10 }}
+          style={{ marginTop: 10 }}
+        />
+      </Card.Actions>
+    </Card>
+  );
+});
