f061d3fcd9bc79712cfb042c6e71f216d54146c5
lakindu hewawasam <lakindu2002@live.com>
2022-09-24 12:15:51 +0530

    adding custom styling

--- src/common/OutlinedTextInput.tsx
+++ src/common/OutlinedTextInput.tsx
@@ -1,4 +1,5 @@
 import React, { FC, Fragment } from 'react';
+import { View } from 'react-native';
 import { HelperText, TextInput } from 'react-native-paper';
 import { TextInputProps } from 'react-native-paper/lib/typescript/components/TextInput/TextInput';
 
@@ -6,9 +7,13 @@ interface OutlinedTextInputProps extends Partial<TextInputProps> {
   helperText?: string | false | undefined;
 }
 
-export const OutlinedTextInput: FC<OutlinedTextInputProps> = ({ helperText, ...props }) => {
+export const OutlinedTextInput: FC<OutlinedTextInputProps> = ({ helperText, style, ...props }) => {
   return (
-    <Fragment>
+    <View
+      {...style && {
+        style: { ...style as any }
+      }}
+    >
       <TextInput {...props}
         mode='outlined'
       />{
@@ -19,6 +24,6 @@ export const OutlinedTextInput: FC<OutlinedTextInputProps> = ({ helperText, ...p
         )
       }
 
-    </Fragment>
+    </View>
   );
 };
