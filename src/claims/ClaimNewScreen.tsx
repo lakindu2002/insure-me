6ed022a2c0c1713f0b1c31959ff0d2d0c674dd9d
lakindu hewawasam <lakindu2002@live.com>
2022-09-24 12:15:17 +0530

    implemented creating a claim

--- src/claims/ClaimNewScreen.tsx
+++ src/claims/ClaimNewScreen.tsx
@@ -1,8 +1,20 @@
+import { useAuth } from '@insureme/auth/AuthContext';
+import { Alert } from '@insureme/common/Alert';
 import { CustomButton } from '@insureme/common/CustomButton';
+import { DropdownMenu } from '@insureme/common/DropdownMenu';
+import { globalStyles } from '@insureme/common/GlobalStyles';
+import { Image } from '@insureme/common/Image';
+import { OutlinedTextInput } from '@insureme/common/OutlinedTextInput';
+import { TimePicker } from '@insureme/common/TimePicker';
+import { useCamera } from '@insureme/common/UseCamera';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { FC, useEffect, useState } from 'react';
-import { ScrollView, View } from 'react-native';
+import { FC, useCallback, useEffect, useMemo, useState } from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { Divider, IconButton, Provider, Text, useTheme } from 'react-native-paper';
+import { useToast } from 'react-native-toast-notifications';
+import { AccidentType, Claim } from './Claim.type';
 import { ClaimNavigatorParamList } from './ClaimNavigator';
+import { useClaims } from './ClaimsContext';
 
 type ClaimNewScreenNavigatorProps = NativeStackScreenProps<
   ClaimNavigatorParamList,
@@ -11,35 +23,91 @@ type ClaimNewScreenNavigatorProps = NativeStackScreenProps<
 
 interface ClaimNewScreenProps extends ClaimNewScreenNavigatorProps { }
 
-const DetailsStep = () => {
+const Header: FC<{ label: string }> = ({ label }) => {
   return (
     <View>
-
-    </View>
-  );
-};
-
-const DocumentsStep = () => {
-  return (
-    <View>
-
+      <Text style={{
+        fontSize: 18,
+        fontWeight: '700'
+      }}>
+        {label}
+      </Text>
+      <Divider style={{ marginVertical: 10 }} />
     </View>
   )
-};
+}
 
 const steps = [
   {
     title: 'Provide Claim Information',
-    component: DetailsStep,
   },
   {
     title: 'Provide Claim Documents',
-    component: DocumentsStep,
   }
 ]
 
+const styles = StyleSheet.create({
+  marginBottom: {
+    marginBottom: 20,
+  }
+});
+
+const validateStep01 = (claim: Partial<Claim>) => {
+  const validationErrors: { key: string, message: string }[] = [];
+  if (!claim.accidentType) validationErrors.push({ key: 'accidentType', message: 'Accident type is required' });
+  if (!claim.time) validationErrors.push({ key: 'time', message: 'Accident time is required' });
+  if (!claim.expectedCurrency) validationErrors.push({ key: 'expectedCurrency', message: 'Expected currency is required' });
+  if (!claim.expectedAmount) validationErrors.push({ key: 'expectedAmount', message: 'Expected amount is required' });
+  if (!claim.vehicle) validationErrors.push({ key: 'vehicle', message: 'Vehicle is required' });
+  if (!claim.location) validationErrors.push({ key: 'location', message: 'Location is required' });
+  if (!claim.owner?.name) validationErrors.push({ key: 'ownerName', message: 'Your name is required. Please update it in the profile' });
+  if (!claim.owner?.nic) validationErrors.push({ key: 'nic', message: 'Your NIC is required. Please update it in the profile' });
+  if (!claim.owner?.phone) validationErrors.push({ key: 'contact', message: 'Your contact number is required. Please update it in the profile' });
+  return validationErrors;
+}
+
+const validateStep02 = (images: string[]) => {
+  const validationErrors: { key: string, message: string }[] = [];
+  if (images.length === 0) validationErrors.push({ key: 'images', message: 'At least one image is required' });
+  return validationErrors;
+}
+
 export const ClaimNewScreen: FC<ClaimNewScreenProps> = ({ navigation }) => {
   const [selectedStep, setSelectedStep] = useState<number>(0);
+  const { accidentTypes, getVehicleAccidentTypes, claimVehicles, createClaim, claimCreating } = useClaims();
+  const { images, launchPhotoSelection } = useCamera(5);
+  const [newClaim, setNewClaim] = useState<Partial<Claim>>({ time: Date.now() });
+
+  const [step01ValidationErrors, setStep01ValidationErrors] = useState<{ key: string, message: string }[]>([]);
+  const [step02ValidationErrors, setStep02ValidationErrors] = useState<{ key: string, message: string }[]>([]);
+
+  const theme = useTheme();
+  const toast = useToast();
+
+  const { user } = useAuth();
+
+  useEffect(() => {
+    setNewClaim((claim) => ({
+      ...claim, pictures: [...(claim.pictures || []), ...images.map(image => image.path)]
+    }))
+  }, [images]);
+
+  useEffect(() => {
+    if (!user) {
+      return;
+    }
+    setNewClaim((claim) => ({
+      ...claim, owner: {
+        name: user.fullName,
+        phone: user.contact || '',
+        nic: user.nicImageUrl || '',
+      }
+    }))
+  }, [user]);
+
+  useEffect(() => {
+    getVehicleAccidentTypes();
+  }, []);
 
   useEffect(() => {
     navigation.setOptions({
@@ -48,48 +116,277 @@ export const ClaimNewScreen: FC<ClaimNewScreenProps> = ({ navigation }) => {
   }, [selectedStep]);
 
   const getPositiveLabel = () => {
-    if (selectedStep === steps.length) {
+    if (selectedStep === steps.length - 1) {
       return 'Submit Claim';
     }
     return 'Next'
   }
 
-  const handlePositiveAction = () => {
+  const handlePositiveAction = async () => {
     if (selectedStep === 0) {
-      setSelectedStep(1);
+      const validationResults = validateStep01(newClaim);
+      setStep01ValidationErrors(validationResults);
+      if (validationResults.length === 0) {
+        setSelectedStep(1);
+      }
+      return;
+    }
+    const validationResults = validateStep02(newClaim.pictures || []);
+    setStep02ValidationErrors(validationResults);
+    if (validationResults.length !== 0) {
       return;
     }
     // create claim
+    const created = await createClaim(newClaim);
+    if (created) {
+      navigation.navigate('ClaimList');
+    }
   };
 
+  const handleVehicleSelect = useCallback((selectedVehicleChassisId: string) => {
+    const selectedVehicle = claimVehicles.find((v) => v.chassisNumber === selectedVehicleChassisId);
+    if (!selectedVehicle) {
+      toast.show('Vehicle does not exist', { type: 'danger' });
+    } else {
+      setNewClaim((claim) => ({ ...claim, vehicle: selectedVehicle }));
+    }
+  }, [claimVehicles])
+
+  const handleExpectedClaimAmountChange = (newClaimAmount: string) => {
+    if (isNaN(Number(newClaimAmount))) {
+      return;
+    }
+    setNewClaim((claim) => ({ ...claim, expectedAmount: Number(newClaimAmount) }));
+  }
+
+  const launchCamera = () => {
+    launchPhotoSelection();
+  }
+
+  const handleImageRemove = (index: number) => {
+    const newImages = [...newClaim.pictures || []];
+    newImages.splice(index, 1);
+    setNewClaim((claim) => ({ ...claim, pictures: newImages }));
+  }
+
+  const handleTimeChanged = (newTime: Date) => {
+    const newTimeInMs = newTime.getTime();
+    setNewClaim((claim) => ({ ...claim, time: newTimeInMs }));
+  }
+
+  const selectedVehicle = useMemo(() => claimVehicles.find((vehicle) => vehicle.chassisNumber === newClaim.vehicle?.chassisNumber), [newClaim.vehicle?.chassisNumber]);
   return (
-    <ScrollView>
-      <View
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'flex-end',
-          paddingHorizontal: 20,
-        }}
+    <ScrollView style={{
+      ...globalStyles.container,
+      backgroundColor: theme.colors.background,
+    }}>
+      <Provider
+        theme={theme}
       >
-        {
-          selectedStep === 1 && <CustomButton
-            label={'Back'}
-            color='error'
-            onPress={() => setSelectedStep(0)}
-            mode='outlined'
+        <View
+          style={{
+            padding: 20,
+          }}
+        >
+          {
+            selectedStep === 0 && (
+              <>
+                <Header label={'Accident Information'} />
+                <View style={styles.marginBottom}>
+                  <DropdownMenu
+                    label='Accident Type'
+                    items={accidentTypes}
+                    error={step01ValidationErrors.find((error) => error.key === 'accidentType') !== undefined}
+                    helperText={step01ValidationErrors.find((error) => error.key === 'accidentType')?.message}
+                    selected={accidentTypes.find((item) => item.value === newClaim.accidentType)}
+                    onSelect={(item) => setNewClaim((claim) => ({ ...claim, accidentType: item as AccidentType }))}
+                  />
+                </View>
+                <View style={styles.marginBottom}>
+                  <TimePicker
+                    initialValue={newClaim.time}
+                    onDateChange={handleTimeChanged}
+                  />
+                </View>
+                <View style={styles.marginBottom}>
+                  <DropdownMenu
+                    label='Vehicle'
+                    helperText={step01ValidationErrors.find((error) => error.key === 'vehicle')?.message}
+                    error={step01ValidationErrors.find((error) => error.key === 'vehicle') !== undefined}
+                    items={claimVehicles.map((vehicle) => ({ label: `${vehicle.brand} ${vehicle.model}`, value: vehicle.chassisNumber }))}
+                    selected={selectedVehicle && { label: `${selectedVehicle.brand} ${selectedVehicle.model}`, value: selectedVehicle.chassisNumber }}
+                    onSelect={(newChassisNumber: string) => handleVehicleSelect(newChassisNumber)}
+                  />
+                </View>
+                <View style={[styles.marginBottom, {
+                  display: 'flex',
+                  flexDirection: 'row',
+                }]}>
+                  <OutlinedTextInput
+                    label={'Currency'}
+                    style={{
+                      flex: 1,
+                      marginRight: 10
+                    }}
+                    helperText={step01ValidationErrors.find((error) => error.key === 'expectedCurrency')?.message}
+                    error={step01ValidationErrors.find((error) => error.key === 'expectedCurrency') !== undefined}
+                    value={newClaim.expectedCurrency || ''}
+                    onChangeText={(newCurrency) => setNewClaim((claim) => ({ ...claim, expectedCurrency: newCurrency }))}
+                  />
+                  <OutlinedTextInput
+                    style={{
+                      flex: 2
+                    }}
+                    label={'Expected Claim Amount'}
+                    helperText={step01ValidationErrors.find((error) => error.key === 'expectedAmount')?.message}
+                    error={step01ValidationErrors.find((error) => error.key === 'expectedAmount') !== undefined}
+                    value={(newClaim.expectedAmount || 0).toFixed()}
+                    onChangeText={handleExpectedClaimAmountChange}
+                  />
+                </View>
+                <View style={styles.marginBottom}>
+                  <OutlinedTextInput
+                    label={'Location'}
+                    multiline
+                    numberOfLines={4}
+                    helperText={step01ValidationErrors.find((error) => error.key === 'location')?.message}
+                    error={step01ValidationErrors.find((error) => error.key === 'location') !== undefined}
+                    value={newClaim.location || ''}
+                    onChangeText={(newLocation) => setNewClaim((claim) => ({ ...claim, location: newLocation }))}
+                  />
+                </View>
+                <Header label={'Person Information'} />
+                <View style={styles.marginBottom}>
+                  <OutlinedTextInput
+                    editable={false}
+                    label={'Person Name'}
+                    helperText={step01ValidationErrors.find((error) => error.key === 'ownerName')?.message}
+                    error={step01ValidationErrors.find((error) => error.key === 'ownerName') !== undefined}
+                    value={newClaim.owner?.name || ''}
+                  />
+                </View>
+                <View style={styles.marginBottom}>
+                  <OutlinedTextInput
+                    editable={false}
+                    label={'Phone Number'}
+                    value={newClaim.owner?.phone || ''}
+                    error={!newClaim.owner?.phone}
+                    helperText={!newClaim.owner?.phone && 'Please add a phone number in your profile'}
+                  />
+                </View>
+                <Text
+                  style={{
+                    fontWeight: '700',
+                    marginBottom: 10,
+                    fontSize: 16
+                  }}
+                >
+                  NIC
+                </Text>
+                {
+                  newClaim.owner?.nic ? (
+                    <Image
+                      source={{ uri: newClaim.owner?.nic || '' }}
+                    />
+                  ) : (
+                    <Alert
+                      message='Visit the profile screen to upload your NIC'
+                      title='NIC Not Provided'
+                      variant='error'
+                    />
+                  )
+                }
+              </>
+            )
+          }
+          {
+            selectedStep === 1 && (
+              <>
+                <Header
+                  label='Upload Vehicular Damage'
+                />
+                {
+                  (newClaim.pictures || []).length < 5 && (
+                    <>
+                      <Text>
+                        Select/take upto 5 images of the vehicular damage
+                      </Text>
+                      <View
+                        style={{
+                          display: 'flex',
+                          alignItems: 'center',
+                          width: '100%',
+                          marginTop: 20
+                        }}
+                      >
+                        <IconButton
+                          onPress={launchCamera}
+                          icon={'camera'}
+                        />
+                      </View>
+                      <Divider style={{ marginVertical: 20 }} />
+                    </>
+                  )
+                }
+                {(newClaim.pictures || []).length === 0 && (
+                  <Alert
+                    variant='warning'
+                    message='No images have been added'
+                    title='No Images'
+                  />
+                )}
+                {
+                  (newClaim.pictures || []).map((image, index) => (
+                    <View
+                      key={index}
+                      style={[styles.marginBottom, { position: 'relative' }]}
+                    >
+                      <Image
+                        source={{ uri: image }}
+                      />
+                      <IconButton
+                        icon={'close'}
+                        onPress={() => handleImageRemove(index)}
+                        style={{
+                          position: 'absolute',
+                          backgroundColor: theme.colors.primary,
+                        }}
+                      />
+                    </View>
+                  ))
+                }
+              </>
+            )
+          }
+          <View
             style={{
-              marginRight: 15
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'flex-end',
+              marginTop: 20
             }}
-          />
-        }
-        <CustomButton
-          mode='contained'
-          color='primary'
-          onPress={handlePositiveAction}
-          label={getPositiveLabel()}
-        />
-      </View>
+          >
+            {
+              selectedStep === 1 && <CustomButton
+                label={'Back'}
+                color='error'
+                onPress={() => setSelectedStep(0)}
+                mode='outlined'
+                style={{
+                  marginRight: 15
+                }}
+              />
+            }
+            <CustomButton
+              mode='contained'
+              color='primary'
+              loading={claimCreating}
+              onPress={handlePositiveAction}
+              label={getPositiveLabel()}
+            />
+          </View>
+        </View>
+      </Provider>
     </ScrollView>
   );
 };
--- src/claims/ClaimsContext.tsx
+++ src/claims/ClaimsContext.tsx
@@ -1,31 +1,47 @@
 import React, { createContext, FC, useCallback, useContext, useEffect, useReducer, useState } from 'react';
-import { Claim, ClaimStatus } from './Claim.type';
+import { AccidentType, Claim, ClaimStatus, ClaimVehicle } from './Claim.type';
 import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
 import { useAuth } from '@insureme/auth/AuthContext';
 import { UserRole } from '@insureme/auth/User.type';
 import { useToast } from 'react-native-toast-notifications';
+import { getAccidentTypeName } from './Claim.util';
 
 const claimRef = firestore().collection('claims');
+const vehicleRef = firestore().collection('vehicles');
+const claimsImagesRef = storage().ref('claims');
 
 interface State {
   claims: Claim[],
   selectedClaimStatus: ClaimStatus,
   claimsLoading: boolean,
+  accidentTypes: { label: string, value: string }[]
+  claimVehicles: ClaimVehicle[]
+  claimCreating: boolean
 }
 
 const initialState: State = {
   claims: [],
   selectedClaimStatus: ClaimStatus.PENDING,
   claimsLoading: false,
+  accidentTypes: [],
+  claimVehicles: [],
+  claimCreating: false
 }
 
 interface ClaimsContextType extends State {
   updateSelectedClaimStatus: (status: ClaimStatus) => void;
+  getVehicleAccidentTypes: () => void
+  getClaimVehicles: () => Promise<void>,
+  createClaim: (newClaim: Partial<Claim>) => Promise<boolean>
 };
 
 const ClaimsContext = createContext<ClaimsContextType>({
   ...initialState,
   updateSelectedClaimStatus: () => { },
+  getVehicleAccidentTypes: () => { },
+  getClaimVehicles: () => Promise.resolve(),
+  createClaim: () => Promise.resolve(true)
 });
 
 interface ClaimsProviderProps {
@@ -47,7 +63,27 @@ type SET_CLAIMS_ACTION = {
   payload: Claim[];
 }
 
-type Action = UPDATE_SELECTED_CLAIM_STATUS_ACTION | UPDATE_LOADING_CLAIMS_ACTION | SET_CLAIMS_ACTION;
+type SET_VEHICLE_ACCIDENT_TYPES_ACTION = {
+  type: 'SET_VEHICLE_ACCIDENT_TYPES';
+  payload: { label: string, value: string }[];
+}
+
+type SET_CLAIM_VEHICLES = {
+  type: 'SET_CLAIM_VEHICLES';
+  payload: ClaimVehicle[];
+}
+
+type ADD_CLAIM = {
+  type: 'ADD_CLAIM',
+  payload: Claim
+}
+
+type SET_CLAIM_CREATING_ACTION = {
+  type: 'SET_CLAIM_CREATING';
+  payload: boolean;
+}
+
+type Action = UPDATE_SELECTED_CLAIM_STATUS_ACTION | UPDATE_LOADING_CLAIMS_ACTION | SET_CLAIMS_ACTION | SET_VEHICLE_ACCIDENT_TYPES_ACTION | SET_CLAIM_VEHICLES | ADD_CLAIM | SET_CLAIM_CREATING_ACTION;
 
 const reducer = (state: State, action: Action): State => {
   switch (action.type) {
@@ -65,6 +101,26 @@ const reducer = (state: State, action: Action): State => {
       return {
         ...state,
         claims: action.payload,
+      }
+    case 'SET_VEHICLE_ACCIDENT_TYPES':
+      return {
+        ...state,
+        accidentTypes: action.payload,
+      }
+    case 'SET_CLAIM_VEHICLES':
+      return {
+        ...state,
+        claimVehicles: action.payload,
+      }
+    case 'ADD_CLAIM':
+      return {
+        ...state,
+        claims: [...state.claims, action.payload]
+      }
+    case 'SET_CLAIM_CREATING':
+      return {
+        ...state,
+        claimCreating: action.payload
       }
     default:
       return state;
@@ -121,12 +177,86 @@ export const ClaimsProvider: FC<ClaimsProviderProps> = ({ children }) => {
     }
   }, [loadClaimsByMode]);
 
+  const getVehicleAccidentTypes = useCallback(() => {
+    const data = [
+      { label: getAccidentTypeName(AccidentType.HEAD_ON), value: AccidentType.HEAD_ON },
+      { label: getAccidentTypeName(AccidentType.HIT_AND_RUN), value: AccidentType.HIT_AND_RUN },
+      { label: getAccidentTypeName(AccidentType.LOW_SPEED), value: AccidentType.LOW_SPEED },
+      { label: getAccidentTypeName(AccidentType.REAR), value: AccidentType.REAR },
+      { label: getAccidentTypeName(AccidentType.ROLLOVER), value: AccidentType.ROLLOVER },
+      { label: getAccidentTypeName(AccidentType.SIDE_IMPACT), value: AccidentType.SIDE_IMPACT },
+    ]
+    dispatch({ type: 'SET_VEHICLE_ACCIDENT_TYPES', payload: data });
+  }, []);
+
+  const getClaimVehicles = useCallback(async () => {
+    dispatch({ type: 'SET_CLAIM_VEHICLES', payload: [] });
+    const snapshot = await vehicleRef.where('owner', '==', user?.id).get();
+    if (!snapshot.empty) {
+      const data = snapshot.docs.map((doc) => doc.data()).map((vehicle) => ({ brand: vehicle.brand, chassisNumber: vehicle.chassisNumber, licensePlate: vehicle.licensePlate, model: vehicle.model, pictureUrl: vehicle.pictureUrl }) as ClaimVehicle);
+      dispatch({ type: 'SET_CLAIM_VEHICLES', payload: data });
+    }
+  }, []);
+
+  const createClaim = useCallback(async (newClaim: Partial<Claim>) => {
+    const timestamp = firestore.Timestamp.now().toMillis();
+    const picturePathsToUpload = newClaim.pictures as string[];
+    dispatch({ type: 'SET_CLAIM_CREATING', payload: true });
+    try {
+      const newDocumentRef = claimRef.doc();
+
+      const claimId = newDocumentRef.id;
+      // all or nothing
+      const uploadedPictureUrls = await Promise.all(picturePathsToUpload.map(async (path, idx) => {
+        const imageKey = `${claimId}/${idx}`;
+        const imageRef = claimsImagesRef.child(imageKey);
+        await imageRef.putFile(path);
+        const url = await imageRef.getDownloadURL();
+        return url;
+      }));
+
+      const claim: Claim = {
+        id: claimId,
+        accidentType: newClaim.accidentType as AccidentType,
+        createdAt: timestamp,
+        updatedAt: timestamp,
+        expectedAmount: newClaim.expectedAmount as number,
+        expectedCurrency: newClaim.expectedCurrency as string,
+        location: newClaim.location as string,
+        status: ClaimStatus.PENDING,
+        time: newClaim.time as number,
+        ownerId: user?.id as string,
+        owner: {
+          name: user?.fullName as string,
+          nic: user?.nicImageUrl as string,
+          phone: user?.contact as string,
+        },
+        pictures: uploadedPictureUrls,
+        vehicle: newClaim.vehicle as ClaimVehicle,
+      }
+      await claimRef.doc(claimId).set(claim);
+      dispatch({ type: 'ADD_CLAIM', payload: claim });
+      return true;
+    } catch (err) {
+      showToast('Error creating claim', { type: 'danger' });
+      return false;
+    } finally {
+      dispatch({ type: 'SET_CLAIM_CREATING', payload: false });
+    }
+  }, []);
+
   return (
     <ClaimsContext.Provider value={{
       claims: state.claims,
       selectedClaimStatus: state.selectedClaimStatus,
       updateSelectedClaimStatus,
-      claimsLoading: state.claimsLoading
+      claimsLoading: state.claimsLoading,
+      accidentTypes: state.accidentTypes,
+      getVehicleAccidentTypes,
+      claimVehicles: state.claimVehicles,
+      getClaimVehicles,
+      claimCreating: state.claimCreating,
+      createClaim
     }}>
       {children}
     </ClaimsContext.Provider>
